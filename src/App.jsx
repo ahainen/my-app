51cf9c0f30d16a6804a16c2bc4d09209fd577aea
ahainen <ahainen@gmail.com>
2023-09-10 09:30:07 -0400

    tailwind added in

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+
+function MyButton() {
+  return (
+    <button className='p-12'>I'm a button</button>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       
+      <MyButton />
+      <MyButton />
+      <MyButton />
+
+
     </div>
   );
 }
