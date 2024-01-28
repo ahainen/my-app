fe8c34990800a8d36dd0cd37c72d9b8960a71963
ahainen <ahainen@gmail.com>
2024-01-28 15:08:08 -0500

    adding in rive, or trying to

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import './App.css';
+import { Rive } from 'rive-react';
 
 function MyButton() {
   return (
-    <button className='p-12'>I'm a button</button>
+    <button className='px-5 py-3 bg-blue-600 hover:bg-blue-700 text-white font-bold'>I'm a button</button>
   );
 }
 
 function App() {
   return (
     <div className="App">
-      
-      <MyButton />
-      <MyButton />
-      <MyButton />
+      <div className='flex'>
+        <MyButton />
+        <MyButton />
+        <MyButton />
+      </div>
+      <div>
+          
+            <Rive src="switch.riv" />
+            
+      </div>
 
 
     </div>
